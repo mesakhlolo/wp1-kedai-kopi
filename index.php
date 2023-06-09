6d296d0f530ebf5e03e3fd83852d426e16bab6ac
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-06-09 19:54:51 +0700

    menambahkan img

--- index.php
+++ index.php
@@ -11,7 +11,12 @@
     <?php include 'partials/header.php'; ?>
 
     <div class="main-container">
-        <h1>Selamat datang di Kedai Kopi Paste!</h1>
+        <h1>Mari Nikmati Secangkir <span>Kopi</span></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
+            sapiente.
+        </p>
+        <a href="#" class="cta">Beli Sekarang</a>
     </div>
 
     <?php include 'partials/footer.php'; ?>
