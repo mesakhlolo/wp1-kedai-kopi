7bdd2377f141b552212675fb7c3ed60827d4afce
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-06-09 19:21:27 +0700

    menambahkan logo dan memperbaiki navbar

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,3 @@
-<footer>
+<footer class="footer">
     <p> <span>Kedai Kopi Paste</span> &copy; 2023. All rights reserved.</p>
 </footer>
--- partials/header.php
+++ partials/header.php
@@ -1,8 +1,12 @@
 <header>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="tentang_kami.php">Tentang Kami</a>
-        <a href="pesan.php">Pesan</a>
-        <a href="kontak.php">Kontak</a>
+    <nav class="navbar">
+        <a href="#" class="navbar-logo">kopi<span>paste</span>.</a>
+
+        <div class="navbar-nav">
+            <a href="index.php">Home</a>
+            <a href="tentang_kami.php">Tentang Kami</a>
+            <a href="pesan.php">Pesan</a>
+            <a href="kontak.php">Kontak</a>
+        </div>
     </nav>
 </header>
