3334178f80b95ebc48435f2ad01202fbbd5e6131
Mesakh Lolo Meha <mesakhlolo12@gmail.com>
2023-06-21 16:07:46 +0700

    menyelesaikan project

--- kontak.php
+++ kontak.php
@@ -5,17 +5,33 @@
     <title>Kedai Kopi Paste - Kontak</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/kontak.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <?php include 'partials/header.php'; ?>
 
     <div class="main-container">
-        <h1>Kontak Kami</h1>
+        <div class="contact-form">
+            <h1>Kontak Kami</h1>
 
-        <p>Hubungi kami melalui:</p>
-        <p>Email: kedaikopi@paste.com</p>
-        <p>Telepon: 0123456789</p>
+            <form action="proses_kontak.php" method="POST">
+                <label for="name">Nama:</label>
+                <input type="text" id="name" name="name" required>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="message">Pesan:</label>
+                <textarea id="message" name="message" required></textarea>
+
+                <input type="submit" value="Kirim">
+            </form>
+
+            <div class="wa-contact">
+                <p>Atau hubungi kami melalui <a href="https://www.whatsapp.com/" target="_blank"><i class="fa fa-whatsapp"></i></a></p>
+            </div>
+        </div>
     </div>
 
 
